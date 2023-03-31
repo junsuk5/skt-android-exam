05438b6fd6541cc1c44e5f8ccc7d51083ada8e50
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-03-31 09:55:18 +0900

    add: Kotlin Lambda 문제

--- app/src/main/java/com/surivalcoding/androidexam/day1/Stream.kt
+++ app/src/main/java/com/surivalcoding/androidexam/day1/Stream.kt
@@ -1,7 +1,25 @@
 package com.surivalcoding.androidexam.day1
 
 fun main() {
-    val items = listOf(1, 2, 3, 4, 5, 6, 6, 6)
+    // 0
+    val items = listOf(1, 2, 3, 4, 5)
+
+    println(items.first().isEven())
+    println(items.first().isOdd())
+
+    println(items.sum())
+
+    println(items.any { it > 10 })
+
+    // 1, 2, 3, 4, 5
+    // 3, 3, 4, 5
+    // 6, 4, 5
+    // 10, 5
+    // 15
+    println(items.fold(0) { sum, num -> sum + num })
+    println(items.reduce { sum, num -> sum + num })
+    println(items.fold(1) { sum, num -> sum * num })
+    println(items.reduce { sum, num -> sum * num })
 
     println(items)
 
@@ -16,14 +34,12 @@ fun main() {
     println(items.take(2))
 
     // 아무 생각없이 처음부터 끝까지 돌린다
-    items.forEach { item ->
-        println(item)
-    }
+    items.forEach { println(it) }
 
     println(
-        items.filter { item -> item % 2 == 0 }  // if
-            .map { item -> item + 1 }   // 바꾸는거
-            .map { item -> "${item * 2}등 " }
+        items.filter { it % 2 == 0 }  // if
+            .map { it + 1 }   // 바꾸는거
+            .map { "${it * 2}등 " }
     )
 
     // for 문 대신에
@@ -31,4 +47,13 @@ fun main() {
 //        println("Hello")
 //    }
 //    println(items)
+}
+
+// 확장함수 extension function
+fun Int.isEven(): Boolean {
+    return this % 2 == 0
+}
+
+fun Int.isOdd(): Boolean {
+    return !isEven()
 }
